2e027c76f85beb23b88376afb0d3d8271b42693b
sunilmittal1983 <46190172+sunilmittal1983@users.noreply.github.com>
2019-01-05 21:18:14 +0530

    Add files via upload

---
+++ ChallanEdit.aspx.cs
@@ -0,0 +1,30 @@
+﻿using Samruddhi.Utilities;
+using System;
+using System.Collections;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Samruddhi
+{
+    public partial class ChallanEdit : System.Web.UI.Page
+    {
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {            
+            if (!System.Web.HttpContext.Current.User.Identity.IsAuthenticated)
+                Response.Redirect("Login.aspx");            
+        }    
+       
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            var challanno = String.Format("{0}", Request.Form["chrefno"]);
+            Response.Redirect("ChallanForm.aspx?ChallanNumber =" + challanno);
+        }
+    }
+}
